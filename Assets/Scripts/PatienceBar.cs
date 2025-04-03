dea14d2fadf622ada7aeaddff6f6a738ffac9b94
Guruprasath <guruprasath3200@gmail.com>
2025-04-03 09:21:35 +0800

    Added Gradient for Patience Bar' '

--- Assets/Scripts/PatienceBar.cs
+++ Assets/Scripts/PatienceBar.cs
@@ -6,15 +6,19 @@ public class PatienceBar : MonoBehaviour
 {
     public Slider slider;
 
+    public Gradient gradient;
+    public Image fill;
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
+        fill.color = gradient.Evaluate(1f);
     }
 
     public void SetHeath(int health)
     {
         slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 
 }
